f480207097a76e2fa2fc6164a3e8b516e47f002f
Brian Pursley <bpursley@cinlogic.com>
2016-03-24 17:25:44 -0400

    Refactored gulpfile and removed images

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var uglify = require('gulp-uglify');
 var nodemon = require('gulp-nodemon');
 var htmlmin = require('gulp-htmlmin');
 var angularTemplateCache = require('gulp-angular-templatecache');
+var gutil = require('gulp-util');
 var addStream = require('add-stream');
 
 function prepareTemplates() {
@@ -17,15 +18,8 @@ gulp.task('app', function() {
   return gulp.src('./client/app/**/*.js')
     .pipe(addStream.obj(prepareTemplates()))
     .pipe(concat('app.js'))
-    .pipe(ngAnnotate())
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('app-dev', function() {
-  return gulp.src('./client/app/**/*.js')
-    .pipe(addStream.obj(prepareTemplates()))
-    .pipe(concat('app.js'))
+    .pipe(gutil.env.type === 'production' ? ngAnnotate() : gutil.noop())
+    .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
     .pipe(gulp.dest('./dist'));
 });
 
@@ -34,15 +28,9 @@ gulp.task('assets', function() {
     .pipe(gulp.dest('./dist/assets'));
 });
 
-gulp.task('watch-app', function(){
-  return gulp.watch([
-    './client/app/**/*.js',
-    './client/app/**/*.tpl.html'
-  ], ['app-dev']);
-});
-
-gulp.task('watch-assets', function(){
-  return gulp.watch('./client/assets/**/*', ['assets']);
+gulp.task('watch', function(){
+  gulp.watch(['./client/app/**/*.js', './client/app/**/*.tpl.html'], ['app']);
+  gulp.watch('./client/assets/**/*', ['assets']);
 });
 
 gulp.task('nodemon', function() {
@@ -51,27 +39,15 @@ gulp.task('nodemon', function() {
     ext: 'js',
     ignore: [
       'client/',
-      'node_modules/',
-      'bower_components/'
-    ]
-  })
-});
-
-gulp.task('nodemon-dev', function() {
-  return nodemon({
-    script: './server/bin/www',
-    ext: 'js',
-    ignore: [
-      'client/',
-      'node_modules/',
-      'bower_components/'
+      'node_modules/'
     ],
     env: { 'NODE_ENV': 'development' }
   })
 });
 
-gulp.task('watch', ['watch-assets', 'watch-app']);
-
-gulp.task('dev', ['assets', 'app-dev', 'watch', 'nodemon']);
-gulp.task('prod', ['assets', 'app']);
-gulp.task('default', ['dev']);
+gulp.task('default', [
+  'assets',
+  'app',
+  'watch',
+  'nodemon'
+]);
